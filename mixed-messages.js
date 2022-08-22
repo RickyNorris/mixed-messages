1328c27eacf3c86293f3ef2a2048c730f0d6804a
Richard Norris <norris_richard@icloud.com>
2022-08-22 16:54:09 -0400

    started making vars

---
+++ mixed-messages.js
@@ -0,0 +1,6 @@
+const firstWord = ['You', 'They', 'He', 'She', 'Your Mom', 'Your Daddy'];
+
+const secondWord = ['like', 'love', 'hate', 'want to', 'need', 'use'];
+const secondWordPluril = ['likes', 'loves', 'hates', 'wants to', 'needs', 'uses'];
+
+const lastWord = ['bottoming', 'going out'];
